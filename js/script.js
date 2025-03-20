20c2bafa92a1a3fe1f4b5f5bebd47be2917a9a90
default <olivia.ten-den@stu.ocsb.ca>
2025-03-20 16:57:54 +0000

    copied over template

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Olivia Ten Den
+// Created on: Mar 17, 2025
 // This file contains the JS functions for index.html
 
 /**
- * Displays an alert with the message "Hello, World!"
+ * Do basic math.
  */
-function myButtonClicked() {
-  alert("Hello, World!")
+function enterClicked() {
+  // input
+  const streetNumber = parseInt(document.getElementById("street-number").value)
+  const streetName = document.getElementById("street-name").value
+  // output
+  document.getElementById("user-address").innerHTML =
+    "<p>The address is: " + streetNumber + " " + streetName + ".</p>"
 }
